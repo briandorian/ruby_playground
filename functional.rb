c75af3736db5f41220ce2b7dee6c89d32cd0b310
Brian Acosta <brian.acosta@xing.com>
2021-10-20 07:32:02 +0200

    Added exercise to practice with map

--- functional.rb
+++ functional.rb
@@ -21,3 +21,14 @@ def functional_urls(states)
   states.map { |state| urlify(state) }
 end
 puts functional_urls(states).inspect
+
+#exercise
+# Using map, write a function that takes in the states variable and returns an
+# array of URLs of the form https://example.com/<urlified form>
+
+def exercise_of_map(states)
+  urls = []
+  states.map { |state| urls << "https://example.com/"+urlify(state) }
+  urls
+end
+puts exercise_of_map(states).inspect
