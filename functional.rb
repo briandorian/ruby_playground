d0c5c95aae8d9fb9c6f3a81261cfe812a9463ba2
Brian Acosta <brian.acosta@xing.com>
2021-10-20 08:05:41 +0200

    impperative example of reduce

--- functional.rb
+++ functional.rb
@@ -71,3 +71,17 @@ def exercise_select_two(states)
 end
 puts exercise_select_two(states).inspect
 
+# SELECT
+# Imperative solution
+
+numbers = 1..10
+
+# sum: Imperative solution
+def imperative_sum(numbers)
+  total = 0
+  numbers.each do |n|
+    total += n
+  end
+  total
+end
+puts imperative_sum(numbers)
