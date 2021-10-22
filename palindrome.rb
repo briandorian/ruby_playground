cff5ccea459afaf68293b049bbaa9146fff01c3a
Brian Acosta <brian.acosta@xing.com>
2021-10-22 08:01:09 +0200

    Preparing a new class that inherits from Phrase, the new class will also be translating

--- palindrome.rb
+++ palindrome.rb
@@ -12,20 +12,23 @@ end
 
 
 # < is how Ruby indicates that Phrase inherits from String class
-  class Phrase < String
-    #returns true if palindrome, false if not.
+class Phrase < String
+
+    # Returns content for palindrome testing
+  def processed_content
+    self.downcase
+  end
+
+  #returns true if palindrome, false if not.
   def palindrome?
-
-    # Inside a Ruby class, the use of self. is necessary when making attribute assignments,
-    # but otherwise you can leave it off. In particular, confirm that we can write downcase
-    # in place of self.downcase, as shown in Listing 7.11. This may look a little odd at first,
-    # but the practice of leaving off self. when possible is common in idiomatic Ruby.
-
-    processed_content = downcase
     processed_content == processed_content.reverse
   end
 
   def louder
     self.content.upcase
   end
+end
+
+class TranslatedPhrase < Phrase
+
 end
