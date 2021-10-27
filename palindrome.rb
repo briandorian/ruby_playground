b35e97885ff800f71f41527f58eed6a4e8548554
Brian Acosta <brian.acosta@xing.com>
2021-10-27 15:16:15 +0200

    Adding the blank in-house made blank method

--- palindrome.rb
+++ palindrome.rb
@@ -9,4 +9,20 @@ class String
   def palindrome?
     processed_content == processed_content.reverse
   end
+
+  def blank?
+    no_spaces = /([\S])/
+    if processed_content.match(no_spaces) == nil
+      return true
+    else
+      return false
+    end
+  end
 end
+
+#Add a blank? method to the String class that returns true if
+# the string is empty or consists solely of whitespace.
+# Hint: Use a regular expression (Section 4.3).
+# You will need the regex syntax for the start and end of a line
+# (Figure 7.12). (The Ruby on Rails framework adds a more advanced
+# version of blank? that handles multiple different kinds of whitespace.)
