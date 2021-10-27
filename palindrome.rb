82e698a29e453006a0cfac6756d87d5a78dbb1c0
Brian Acosta <brian.acosta@xing.com>
2021-10-27 15:22:50 +0200

    Adding the private keyword to the processed content method since it's only used within the class

--- palindrome.rb
+++ palindrome.rb
@@ -1,10 +1,5 @@
 class String
 
-  # Returns content for palindrome testing.
-  def processed_content
-    self.downcase
-  end
-
   # Returns true for a palindrome, false otherwise.
   def palindrome?
     processed_content == processed_content.reverse
@@ -18,11 +13,10 @@ class String
       return false
     end
   end
-end
+    private
 
-#Add a blank? method to the String class that returns true if
-# the string is empty or consists solely of whitespace.
-# Hint: Use a regular expression (Section 4.3).
-# You will need the regex syntax for the start and end of a line
-# (Figure 7.12). (The Ruby on Rails framework adds a more advanced
-# version of blank? that handles multiple different kinds of whitespace.)
+    # Returns content for palindrome testing.
+    def processed_content
+      self.downcase
+    end
+end
