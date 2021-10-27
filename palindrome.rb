b1a273a31bbd145ccd28aa57aa0360cb3d071eaf
Brian Acosta <brian.acosta@xing.com>
2021-10-27 16:02:05 +0200

    Using methods from enumerable to know some stuff about  a range

--- palindrome.rb
+++ palindrome.rb
@@ -2,10 +2,16 @@ require './integer_palindrome.rb'
 require './string_palindrome.rb'
 
 module Palindrome
+  include Enumerable
 
   # Returns true for a palindrome, false otherwise.
   def palindrome?
     processed_content == processed_content.reverse
+  end
+
+  def returnStuff
+    puts "The minimum in the range 1..100 is #{(1..100).minmax[0]}, the maximum is #{(1..100).minmax[1]}
+          and the sum of all the values is #{(1..100).sum}"
   end
 
   private
@@ -15,3 +21,6 @@ module Palindrome
     self.to_s.downcase
   end
 end
+# Using methods from the documentation for the Enumerable module,
+# determine the maximum, minimum, and sum of the range 1..100.
+
