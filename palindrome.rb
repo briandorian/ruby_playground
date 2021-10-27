288d2eb7fd460ffb8df2eae7ccbd98b4a01339d3
Brian Acosta <brian.acosta@xing.com>
2021-10-27 14:46:56 +0200

    Starting point before adding palindrome and processed content to the ruby String class

--- palindrome.rb
+++ palindrome.rb
@@ -1,48 +1,12 @@
-
-
-
-def email_parts(string)
-  mail_before_point = string.partition(".")
-  first_part = mail_before_point[0].partition("@").first
-  server_mail = mail_before_point[0].partition("@")[2]
-  domain_mail = mail_before_point[2]
-
-  "Test #{first_part} - #{server_mail} - #{domain_mail}"
-end
-
-
-# < is how Ruby indicates that Phrase inherits from String class
 class Phrase < String
 
-    # Returns content for palindrome testing
+  # Returns content for palindrome testing.
   def processed_content
     self.downcase
   end
 
-  #returns true if palindrome, false if not.
+  # Returns true for a palindrome, false otherwise.
   def palindrome?
     processed_content == processed_content.reverse
   end
-
-  def louder
-    self.content.upcase
-  end
-end
-
-class TranslatedPhrase < Phrase
-  attr_accessor :translation
-  def initialize(content,translation)
-
-    # This calls the initialize method for the superclass—in this case, Ruby looks for one in Phrase,
-    # but Phrase has no initialize, so Ruby keeps going up the class hierarchy until it finds one in the String class.
-    # At that point, Ruby initializes self to have the value given by the content parameter.
-    super(content)
-    @translation = translation
-  end
-  # overriding
-  # Processes the translation for palindrome testing.
-  def processed_content
-    self.translation.downcase
-  end
-
 end
